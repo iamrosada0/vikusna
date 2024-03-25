92a0afe4f40ef167e82f0283fdb8057106221429
iamrosada0 <luisrosada@outlook.com>
2024-03-25 18:31:48 +0300

    feat: created the item inside of order

---
+++ domain/item.go
@@ -0,0 +1,26 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+)
+
+// Item do Pedido representa um item específico dentro de um pedido
+type OrderItem struct {
+	ID       string `json:"order_item_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	OrderID  string
+	DishID   string
+	Quantity int
+}
+
+func NewOrderItem(orderID, dishID string, quantity int) (*OrderItem, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	return &OrderItem{
+		ID:       id.String(),
+		OrderID:  orderID,
+		DishID:   dishID,
+		Quantity: quantity,
+	}, nil
+}
