07ef86469fa86615574af8828542232a79206fba
iamrosada0 <luisrosada@outlook.com>
2024-03-25 18:44:31 +0300

    feat: created the PaymentTransaction

---
+++ domain/payment_transaction.go
@@ -0,0 +1,24 @@
+package domain
+
+import "github.com/google/uuid"
+
+// Transação de Pagamento representa uma transação de pagamento entre um cliente e um chef
+type PaymentTransaction struct {
+	ID            string
+	OrderID       string
+	Amount        float64
+	PaymentMethod string
+}
+
+func NewPaymentTransaction(orderID, paymentMethod string, amount float64) (*PaymentTransaction, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	return &PaymentTransaction{
+		ID:            id.String(),
+		OrderID:       orderID,
+		Amount:        amount,
+		PaymentMethod: paymentMethod,
+	}, nil
+}
