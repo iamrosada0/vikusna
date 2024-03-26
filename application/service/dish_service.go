4cfcb225d683d32bcb3a3f0063d504dbd90f93cd
iamrosada0 <luisrosada@outlook.com>
2024-03-26 09:38:53 +0300

    feat:Implemented DishService to manage dish creation and retrieval :rocket:

---
+++ application/service/dish_service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"evaeats/application/repositories"
+	"evaeats/domain"
+)
+
+type DishService struct {
+	DishRepository repositories.DishRepository
+}
+
+func NewDishService(dishRepo repositories.DishRepository) *DishService {
+	return &DishService{
+		DishRepository: dishRepo,
+	}
+}
+
+func (s *DishService) CreateDish(chefID, name, description string, price float64, available bool) (*domain.Dish, error) {
+	// Validate input data, if necessary
+
+	// Create the dish in the database
+	newDish, err := s.DishRepository.Insert(chefID, name, description, price, available)
+	if err != nil {
+		return nil, err
+	}
+
+	return newDish, nil
+}
+
+func (s *DishService) GetDishByID(id string) (*domain.Dish, error) {
+	// Fetch the dish by ID from the database
+	dish, err := s.DishRepository.Find(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return dish, nil
+}
