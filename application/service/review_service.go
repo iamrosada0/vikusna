039c8d7f7f597c07d00f87f67fe7211f267bd3e5
iamrosada0 <luisrosada@outlook.com>
2024-03-26 09:38:12 +0300

    feat: Added ReviewService for managing reviews creation and lookup :fire:

---
+++ application/service/review_service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"evaeats/application/repositories"
+	"evaeats/domain"
+)
+
+type ReviewService struct {
+	ReviewRepository repositories.ReviewRepository
+}
+
+func NewReviewService(reviewRepo repositories.ReviewRepository) *ReviewService {
+	return &ReviewService{
+		ReviewRepository: reviewRepo,
+	}
+}
+
+func (s *ReviewService) CreateReview(userID, orderID, dishID, comment string, rating int) (*domain.Review, error) {
+	// Validate input data, if necessary
+
+	// Create the review in the database
+	newReview, err := s.ReviewRepository.Insert(userID, orderID, dishID, comment, rating)
+	if err != nil {
+		return nil, err
+	}
+
+	return newReview, nil
+}
+
+func (s *ReviewService) GetReviewByID(id string) (*domain.Review, error) {
+	// Fetch the review by ID from the database
+	review, err := s.ReviewRepository.Find(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return review, nil
+}
