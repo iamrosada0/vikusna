a72d18cd2a3473d6aba177aa395af4998a80e217
iamrosada0 <luisrosada@outlook.com>
2024-03-26 09:36:45 +0300

    feat: Added UserService with methods for creating and fetching users

---
+++ application/service/user_service.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"evaeats/application/repositories"
+	"evaeats/domain"
+)
+
+type UserService struct {
+	UserRepository repositories.UserRepository
+}
+
+func NewUserService(userRepository repositories.UserRepository) *UserService {
+	return &UserService{
+		UserRepository: userRepository,
+	}
+}
+
+func (s *UserService) CreateUser(userName, email string) (*domain.User, error) {
+	return s.UserRepository.Insert(userName, email)
+}
+
+func (s *UserService) GetUserByID(userID string) (*domain.User, error) {
+	return s.UserRepository.Find(userID)
+}
