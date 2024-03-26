2bf46a0395ebb2ad0927fa67456fd052a52ea379
iamrosada0 <luisrosada@outlook.com>
2024-03-26 12:31:06 +0300

    feat: updated the user struct , added new fields :fire:

--- domain/user.go
+++ domain/user.go
@@ -7,22 +7,36 @@ import (
 )
 
 type User struct {
-	ID        string    `json:"user_id" valid:"uuid" gorm:"type:uuid;primary_key"`
-	Username  string    `json:"user_name" valid:"notnull"`
-	Email     string    `json:"email" valid:"notnull"`
+	ID           string    `json:"user_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	User_name    string    `json:"user_name" valid:"notnull"`
+	Email        string    `json:"email" valid:"notnull"`
+	Password     string    `json:"password"`
+	Phone        string    `json:"phone"`
+	First_name   string    `json:"first_name"`
+	Last_name    string    `json:"last_name"`
+	User_type    string    `json:"user_type" validate:"eq=PRO|eq=CLIENT"`
+	ProfileImage string    `json:"profile_image"`
+	Profile      []Profile `gorm:"profile"`
+
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
 
-func NewUser(user_name, email string) (*User, error) {
+func NewUser(user_name, email, password, phone, first_name, last_name, user_type, profile_image string) (*User, error) {
 	id, err := uuid.NewUUID()
 	if err != nil {
 		return nil, err
 	}
 
 	return &User{
-		ID:       id.String(),
-		Username: user_name,
-		Email:    email,
+		ID:           id.String(),
+		User_name:    user_name,
+		Email:        email,
+		Password:     password,
+		Phone:        phone,
+		First_name:   first_name,
+		Last_name:    last_name,
+		User_type:    user_type,
+		ProfileImage: profile_image,
 	}, nil
 }
