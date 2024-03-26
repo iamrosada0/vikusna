138f86115a0e8be747ca32ddcac201ff4e11f228
iamrosada0 <luisrosada@outlook.com>
2024-03-26 12:43:13 +0300

    feat: added GetUserByEmail and GetUserByPhone into UserService

--- application/services/user_service.go
+++ application/services/user_service.go
@@ -16,10 +16,20 @@ func NewUserService(userRepository repositories.UserRepository) *UserService {
 }
 
 func (s *UserService) CreateUser(user_name, email, password, phone, first_name, last_name, user_type, profile_image string) (*domain.User, error) {
-
 	return s.UserRepository.Insert(user_name, email, password, phone, first_name, last_name, user_type, profile_image)
 }
 
 func (s *UserService) GetUserByID(userID string) (*domain.User, error) {
 	return s.UserRepository.Find(userID)
+}
+
+func (s *UserService) GetUserByEmail(email string) (*domain.User, error) {
+	return s.UserRepository.FindByEmail(email)
+}
+
+func (s *UserService) GetUserByPhone(phone string) (*domain.User, error) {
+	return s.UserRepository.FindByPhone(phone)
+}
+func (s *UserService) UpdateUser(id, user_name, email, password, phone, first_name, last_name, user_type, profile_image string) (*domain.User, error) {
+	return s.UserRepository.Update(id, user_name, email, password, phone, first_name, last_name, user_type, profile_image)
 }
