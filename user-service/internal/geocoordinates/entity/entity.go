fe2c9733bbf0965a3eb34a7baf53c0609c4d91a4
iamrosada0 <luisrosada@outlook.com>
2024-04-01 14:23:58 +0300

    feat: new updates

---
+++ user-service/internal/geocoordinates/entity/entity.go
@@ -0,0 +1,17 @@
+package entity
+
+// CoordenadasGeograficas representa as coordenadas de latitude e longitude
+type GeoCoordinates struct {
+	Name      string  `json:"location_name"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+// NewGeoCoordinates cria e retorna novas coordenadas geográficas
+func NewGeoCoordinates(name string, latitude, longitude float64) (*GeoCoordinates, error) {
+	return &GeoCoordinates{
+		Name:      name,
+		Latitude:  latitude,
+		Longitude: longitude,
+	}, nil
+}
---
+++ user-service/internal/geocoordinates/repository/repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"evaeats/domain"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type GeoCoordinatesRepository interface {
+	Insert(name string, latitude, longitude float64) (*domain.GeoCoordinates, error)
+	Find(id string) (*domain.GeoCoordinates, error)
+}
+
+type GeoCoordinatesRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (repo GeoCoordinatesRepositoryDb) Insert(name string, latitude, longitude float64) (*domain.GeoCoordinates, error) {
+	// Generate a new GeoCoordinates with a generated ID
+	newGeoCoordinates, err := domain.NewGeoCoordinates(name, latitude, longitude)
+	if err != nil {
+		return nil, err
+	}
+
+	// Insert the GeoCoordinates into the database
+	if err := repo.Db.Create(newGeoCoordinates).Error; err != nil {
+		return nil, err
+	}
+
+	return newGeoCoordinates, nil
+}
+
+func (repo GeoCoordinatesRepositoryDb) Find(id string) (*domain.GeoCoordinates, error) {
+	var geoCoordinates domain.GeoCoordinates
+	if err := repo.Db.First(&geoCoordinates, "id = ?", id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("geo coordinates with ID %s not found", id)
+		}
+		return nil, err
+	}
+	return &geoCoordinates, nil
+}
