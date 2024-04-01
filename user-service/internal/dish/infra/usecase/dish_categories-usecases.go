b6469d5877e944c81f48a4486a5188e2d991b814
iamrosada0 <luisrosada@outlook.com>
2024-04-01 09:44:23 +0300

    feat: created dish categories usecases

---
+++ user-service/internal/dish/infra/usecase/dish_categories-usecases.go
@@ -0,0 +1,168 @@
+package usecase
+
+import (
+	"evaeats/user-service/internal/dish/entity"
+)
+
+type CreateDishCategoryUseCase struct {
+	DishCategoryRepository entity.DishCategoryRepository
+}
+
+func NewCreateDishCategoryUseCase(DishCategoryRepository entity.DishCategoryRepository) *CreateDishCategoryUseCase {
+	return &CreateDishCategoryUseCase{DishCategoryRepository: DishCategoryRepository}
+}
+
+func (u *CreateDishCategoryUseCase) Execute(input CreateDishCategoryInputDto) (*CreateDishCategoryOutputDto, error) {
+	// Create a new DishCategory entity using input data
+	newCategory, err := entity.NewDishCategory(input.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	// Call DishCategoryRepository to create the DishCategory
+	err = u.DishCategoryRepository.Create(newCategory)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct output DTO using created DishCategory
+	output := &CreateDishCategoryOutputDto{
+		ID:   newCategory.ID,
+		Name: newCategory.Name,
+	}
+
+	return output, nil
+}
+
+type DeleteDishCategoryUseCase struct {
+	DishCategoryRepository entity.DishCategoryRepository
+}
+
+func NewDeleteDishCategoryUseCase(DishCategoryRepository entity.DishCategoryRepository) *DeleteDishCategoryUseCase {
+	return &DeleteDishCategoryUseCase{DishCategoryRepository: DishCategoryRepository}
+}
+
+func (u *DeleteDishCategoryUseCase) Execute(id string) error {
+	// Call DishCategoryRepository to delete the DishCategory by ID
+	err := u.DishCategoryRepository.DeleteByID(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type UpdateDishCategoryUseCase struct {
+	DishCategoryRepository entity.DishCategoryRepository
+}
+
+func NewUpdateDishCategoryUseCase(DishCategoryRepository entity.DishCategoryRepository) *UpdateDishCategoryUseCase {
+	return &UpdateDishCategoryUseCase{DishCategoryRepository: DishCategoryRepository}
+}
+
+func (u *UpdateDishCategoryUseCase) Execute(input UpdateDishCategoryInputDto) (*UpdateDishCategoryOutputDto, error) {
+	// Retrieve the existing DishCategory
+	existingCategory, err := u.DishCategoryRepository.GetByID(input.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update the existing DishCategory entity
+	existingCategory.Name = input.Name
+
+	// Call DishCategoryRepository to update the DishCategory
+	err = u.DishCategoryRepository.Update(existingCategory)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct output DTO using updated DishCategory
+	output := &UpdateDishCategoryOutputDto{
+		ID:   existingCategory.ID,
+		Name: existingCategory.Name,
+	}
+
+	return output, nil
+}
+
+type GetDishCategoryByIDUseCase struct {
+	DishCategoryRepository entity.DishCategoryRepository
+}
+
+func NewGetDishCategoryByIDUseCase(DishCategoryRepository entity.DishCategoryRepository) *GetDishCategoryByIDUseCase {
+	return &GetDishCategoryByIDUseCase{DishCategoryRepository: DishCategoryRepository}
+}
+
+func (u *GetDishCategoryByIDUseCase) Execute(id string) (*GetDishCategoryByIDOutputDto, error) {
+	// Retrieve the DishCategory by ID
+	category, err := u.DishCategoryRepository.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct output DTO using retrieved DishCategory
+	output := &GetDishCategoryByIDOutputDto{
+		ID:   category.ID,
+		Name: category.Name,
+	}
+
+	return output, nil
+}
+
+type GetAllDishCategoriesUseCase struct {
+	DishCategoryRepository entity.DishCategoryRepository
+}
+
+func NewGetAllDishCategoriesUseCase(DishCategoryRepository entity.DishCategoryRepository) *GetAllDishCategoriesUseCase {
+	return &GetAllDishCategoriesUseCase{DishCategoryRepository: DishCategoryRepository}
+}
+
+func (u *GetAllDishCategoriesUseCase) Execute() ([]*GetAllDishCategoriesOutputDto, error) {
+	// Retrieve all DishCategories
+	categories, err := u.DishCategoryRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct output DTOs using retrieved DishCategories
+	var output []*GetAllDishCategoriesOutputDto
+	for _, category := range categories {
+		output = append(output, &GetAllDishCategoriesOutputDto{
+			ID:   category.ID,
+			Name: category.Name,
+		})
+	}
+
+	return output, nil
+}
+
+// Define input and output DTOs for the use cases
+
+type CreateDishCategoryInputDto struct {
+	Name string `json:"name"`
+}
+
+type CreateDishCategoryOutputDto struct {
+	ID   string `json:"category_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	Name string `json:"name"`
+}
+
+type UpdateDishCategoryInputDto struct {
+	ID   string `json:"category_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	Name string `json:"name"`
+}
+
+type UpdateDishCategoryOutputDto struct {
+	ID   string `json:"category_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	Name string `json:"name"`
+}
+
+type GetDishCategoryByIDOutputDto struct {
+	ID   string `json:"category_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	Name string `json:"name"`
+}
+
+type GetAllDishCategoriesOutputDto struct {
+	ID   string `json:"category_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	Name string `json:"name"`
+}
