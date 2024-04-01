6ba0bcc897fc955b253c9ea4be3a221c6f883ca4
iamrosada0 <luisrosada@outlook.com>
2024-04-01 11:21:56 +0300

    feat: created order item and order

---
+++ user-service/api/order-item.go
@@ -0,0 +1,110 @@
+package api
+
+import (
+	"evaeats/user-service/internal/order/infra/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderItemHandlers struct {
+	CreateOrderItemUseCase  *usecase.CreateOrderItemUseCase
+	ListOrderItemesUseCase  *usecase.GetAllOrdersUseCase
+	DeleteOrderItemUseCase  *usecase.DeleteOrderItemUseCase
+	GetOrderItemByIDUseCase *usecase.GetOrderByIDUseCase
+	UpdateOrderItemUseCase  *usecase.UpdateOrderItemUseCase
+}
+
+func NewOrderItemHandlers(
+	createOrderItemUseCase *usecase.CreateOrderItemUseCase,
+	listOrderItemesUseCase *usecase.GetAllOrdersUseCase,
+	deleteOrderItemUseCase *usecase.DeleteOrderItemUseCase,
+	getOrderItemByIDUseCase *usecase.GetOrderByIDUseCase,
+	updateOrderItemUseCase *usecase.UpdateOrderItemUseCase,
+) *OrderItemHandlers {
+	return &OrderItemHandlers{
+		CreateOrderItemUseCase:  createOrderItemUseCase,
+		ListOrderItemesUseCase:  listOrderItemesUseCase,
+		DeleteOrderItemUseCase:  deleteOrderItemUseCase,
+		GetOrderItemByIDUseCase: getOrderItemByIDUseCase,
+		UpdateOrderItemUseCase:  updateOrderItemUseCase,
+	}
+}
+
+func (dh *OrderItemHandlers) SetupRoutes(router *gin.Engine) {
+	api := router.Group("/api")
+	{
+		OrderItemes := api.Group("/OrderItemes")
+		{
+			OrderItemes.POST("/", dh.CreateOrderItemHandler)
+			OrderItemes.GET("/", dh.ListOrderItemesHandler)
+			OrderItemes.DELETE("/:id", dh.DeleteOrderItemHandler)
+			OrderItemes.GET("/:id", dh.GetOrderItemByIDHandler)
+			OrderItemes.PUT("/:id", dh.UpdateOrderItemHandler)
+		}
+	}
+}
+
+func (dh *OrderItemHandlers) CreateOrderItemHandler(c *gin.Context) {
+	var input usecase.CreateOrderItemInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	output, err := dh.CreateOrderItemUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, output)
+}
+
+func (dh *OrderItemHandlers) ListOrderItemesHandler(c *gin.Context) {
+	output, err := dh.ListOrderItemesUseCase.Execute()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderItemHandlers) DeleteOrderItemHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	input := usecase.DeleteOrderItemInputDto{ID: id}
+	output := dh.DeleteOrderItemUseCase.Execute(input)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderItemHandlers) GetOrderItemByIDHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	input := usecase.GetOrderByIDInputDto{ID: id}
+	output, err := dh.GetOrderItemByIDUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderItemHandlers) UpdateOrderItemHandler(c *gin.Context) {
+	id := c.Param("id")
+	var input usecase.UpdateOrderItemInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	input.ID = id
+
+	output, err := dh.UpdateOrderItemUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
---
+++ user-service/api/order.go
@@ -0,0 +1,110 @@
+package api
+
+import (
+	"evaeats/user-service/internal/order/infra/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderHandlers struct {
+	CreateOrderUseCase  *usecase.CreateOrderUseCase
+	ListOrderesUseCase  *usecase.GetAllOrdersUseCase
+	DeleteOrderUseCase  *usecase.DeleteOrderUseCase
+	GetOrderByIDUseCase *usecase.GetOrderByIDUseCase
+	UpdateOrderUseCase  *usecase.UpdateOrderUseCase
+}
+
+func NewOrderHandlers(
+	createOrderUseCase *usecase.CreateOrderUseCase,
+	listOrderesUseCase *usecase.GetAllOrdersUseCase,
+	deleteOrderUseCase *usecase.DeleteOrderUseCase,
+	getOrderByIDUseCase *usecase.GetOrderByIDUseCase,
+	updateOrderUseCase *usecase.UpdateOrderUseCase,
+) *OrderHandlers {
+	return &OrderHandlers{
+		CreateOrderUseCase:  createOrderUseCase,
+		ListOrderesUseCase:  listOrderesUseCase,
+		DeleteOrderUseCase:  deleteOrderUseCase,
+		GetOrderByIDUseCase: getOrderByIDUseCase,
+		UpdateOrderUseCase:  updateOrderUseCase,
+	}
+}
+
+func (dh *OrderHandlers) SetupRoutes(router *gin.Engine) {
+	api := router.Group("/api")
+	{
+		Orderes := api.Group("/Orderes")
+		{
+			Orderes.POST("/", dh.CreateOrderHandler)
+			Orderes.GET("/", dh.ListOrderesHandler)
+			Orderes.DELETE("/:id", dh.DeleteOrderHandler)
+			Orderes.GET("/:id", dh.GetOrderByIDHandler)
+			Orderes.PUT("/:id", dh.UpdateOrderHandler)
+		}
+	}
+}
+
+func (dh *OrderHandlers) CreateOrderHandler(c *gin.Context) {
+	var input usecase.CreateOrderInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	output, err := dh.CreateOrderUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, output)
+}
+
+func (dh *OrderHandlers) ListOrderesHandler(c *gin.Context) {
+	output, err := dh.ListOrderesUseCase.Execute()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderHandlers) DeleteOrderHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	input := usecase.DeleteOrderInputDto{ID: id}
+	output, err := dh.DeleteOrderUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderHandlers) GetOrderByIDHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	input := usecase.GetOrderByIDInputDto{ID: id}
+	output, err := dh.GetOrderByIDUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
+
+func (dh *OrderHandlers) UpdateOrderHandler(c *gin.Context) {
+	id := c.Param("id")
+	var input usecase.UpdateOrderInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	input.ID = id
+
+	output, err := dh.UpdateOrderUseCase.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, output)
+}
