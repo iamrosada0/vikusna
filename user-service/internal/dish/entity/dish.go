0628dd5786b01f5e018523364be65709b976fad7
iamrosada0 <luisrosada@outlook.com>
2024-04-05 20:22:53 +0300

    feat: added category id

--- user-service/internal/dish/entity/dish.go
+++ user-service/internal/dish/entity/dish.go
@@ -15,16 +15,18 @@ type DishRepository interface {
 }
 
 type Dish struct {
-	ID          string  `json:"dish_id" valid:"uuid" gorm:"type:uuid;primary_key"`
-	ChefID      string  `json:"chef_id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Dish_image  string  `json:"dish_image"`
-	Price       float64 `json:"price"`
-	Available   bool    `json:"available"`
+	ID           string       `json:"dish_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	ChefID       string       `json:"chef_id"`
+	Name         string       `json:"name"`
+	Description  string       `json:"description"`
+	DishImage    string       `json:"dish_image"`
+	Price        float64      `json:"price"`
+	Available    bool         `json:"available"`
+	CategoryID   string       `json:"category_id"`
+	DishCategory DishCategory `gorm:"foreignkey:CategoryID"`
 }
 
-func NewDish(chefID, name, description, dish_image string, price float64, available bool) (*Dish, error) {
+func NewDish(chefID, name, description, dish_image string, price float64, available bool, categoryID string) (*Dish, error) {
 	id, err := uuid.NewUUID()
 	if err != nil {
 		return nil, err
@@ -34,16 +36,17 @@ func NewDish(chefID, name, description, dish_image string, price float64, availa
 		ChefID:      chefID,
 		Name:        name,
 		Description: description,
+		DishImage:   dish_image,
 		Price:       price,
-		Dish_image:  dish_image,
 		Available:   available,
+		CategoryID:  categoryID,
 	}, nil
 }
 
 func (d *Dish) Update(name, description, dishImage string, price float64, available bool) {
 	d.Name = name
 	d.Description = description
-	d.Dish_image = dishImage
+	d.DishImage = dishImage
 	d.Price = price
 	d.Available = available
 }
@@ -56,6 +59,13 @@ func NewInMemoryDishRepository() *InMemoryDishRepository {
 	return &InMemoryDishRepository{
 		Dishes: make(map[string]*Dish),
 	}
+}
+
+func (r *InMemoryDishRepository) Create(Dish *Dish) error {
+	// Implement the logic to create a dish
+	// For an in-memory repository, you would add the dish to the map
+	r.Dishes[Dish.ID] = Dish
+	return nil
 }
 
 func (r *InMemoryDishRepository) DeleteByID(id string) error {
@@ -73,4 +83,19 @@ func (r *InMemoryDishRepository) FindAll() ([]*Dish, error) {
 		allDishes = append(allDishes, dish)
 	}
 	return allDishes, nil
+}
+
+func (r *InMemoryDishRepository) Update(Dish *Dish) error {
+	// Implement the logic to update a dish
+	// For an in-memory repository, you would update the dish in the map
+	r.Dishes[Dish.ID] = Dish
+	return nil
+}
+
+func (r *InMemoryDishRepository) GetByID(id string) (*Dish, error) {
+	dish, exists := r.Dishes[id]
+	if !exists {
+		return nil, errors.New("Dish not found")
+	}
+	return dish, nil
 }
